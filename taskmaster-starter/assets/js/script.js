fec378f9be4ddd2d452b3eb7ca2a71cb77b9630b
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-06-16 15:39:53 -0400

    first bootstrap added

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -45,6 +45,41 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// modal was triggered
+$("#task-form-modal").on("show.bs.modal", function() {
+  // clear values
+  $("#modalTaskDescription, #modalDueDate").val("");
+});
+
+// modal is fully visible
+$("#task-form-modal").on("shown.bs.modal", function() {
+  // highlight textarea
+  $("#modalTaskDescription").trigger("focus");
+});
+
+// save button in modal was clicked
+$("#task-form-modal .btn-primary").click(function() {
+  // get form values
+  var taskText = $("#modalTaskDescription").val();
+  var taskDate = $("#modalDueDate").val();
+
+  if (taskText && taskDate) {
+    createTask(taskText, taskDate, "toDo");
+
+    // close modal
+    $("#task-form-modal").modal("hide");
+
+    // save in tasks array
+    tasks.toDo.push({
+      text: taskText,
+      date: taskDate
+    });
+
+    saveTasks();
+  }
+});
+
+
 // i added this to be able to click on the text area
 $(".list-group").on("click", "p", function() {
   var text = $(this)
@@ -60,36 +95,36 @@ $(".list-group").on("click", "p", function() {
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
 
-  // blur event will trigger as soon as the user interacts with anything other than the <textarea> element
-  $(".list-group").on("blur", "textarea", function(){
-    // get the teatareas current value/text
-    var text = $(this)
-      .val()
-      .trim();
+});
+
+// blur event will trigger as soon as the user interacts with anything other than the <textarea> element
+$(".list-group").on("blur", "textarea", function(){
+  // get the teatareas current value/text
+  var text = $(this)
+    .val();
+    //.trim();
 //debugger; 
-    // get the parent ul id attribute
-    var status = $(this)
-      .closest(".list-group")
-      .attr("id")
-      .replace("list-", "");
+  // get the parent ul id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
 
-    // get the task's postion in the list of other li elements
-    var index = $(this)
-      .closest("list-group-item")
-      .index();
+  // get the task's postion in the list of other li elements
+  var index = $(this)
+    .closest("list-group-item")
+    .index();
 
-    tasks[status][index].text = text;
-    saveTasks();
+  tasks[status][index].text = text;
+  saveTasks();
 
-    //recreata p element
-    var taskP = $("<p>")
-    .addClass("m-1")
-    .text(text);
+  //recreata p element
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
 
-    // replace text area with p element
-    $(this).replaceWith(taskP);
-  });
-
+  // replace text area with p element
+  $(this).replaceWith(taskP);
 });
 
 
@@ -146,41 +181,6 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
 });
 
-
-// modal was triggered
-$("#task-form-modal").on("show.bs.modal", function() {
-  // clear values
-  $("#modalTaskDescription, #modalDueDate").val("");
-});
-
-// modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function() {
-  // highlight textarea
-  $("#modalTaskDescription").trigger("focus");
-});
-
-// save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
-  // get form values
-  var taskText = $("#modalTaskDescription").val();
-  var taskDate = $("#modalDueDate").val();
-
-  if (taskText && taskDate) {
-    createTask(taskText, taskDate, "toDo");
-
-    // close modal
-    $("#task-form-modal").modal("hide");
-
-    // save in tasks array
-    tasks.toDo.push({
-      text: taskText,
-      date: taskDate
-    });
-
-    saveTasks();
-  }
-});
-
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -192,5 +192,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
